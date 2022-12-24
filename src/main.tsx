8a6d8cadb0ba2ef4c832cfbc35f79fd3f51341ef
Hayato Sasaki <h.sasaki.ynu@gmail.com>
2022-12-24 17:03:38 +0900

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Sample } from '@/features/sample'
+import '@/index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Sample />
+  </React.StrictMode>,
+)
