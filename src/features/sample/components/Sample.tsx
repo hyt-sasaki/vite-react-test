46fc2e21e821b1bbd10737f1f6c4f2f6e8b1eb81
Hayato Sasaki <h.sasaki.ynu@gmail.com>
2022-12-25 23:45:15 +0900

    add emotion

--- src/features/sample/components/Sample.tsx
+++ src/features/sample/components/Sample.tsx
@@ -1,22 +1,59 @@
 import React, { useState } from "react";
 import reactLogo from "@/assets/react.svg";
-import "./Sample.css";
+import { css, keyframes } from "@emotion/react";
+
+const rootCss = css({
+  maxWidth: "1280px",
+  margin: "0 auto",
+  padding: "2rem",
+  textAlign: "center",
+});
+const logoCss = css({
+  height: "6em",
+  padding: "1.5em",
+  willChange: "filter",
+});
+const reactLogoSpin = keyframes({
+  from: {
+    transform: "rotate(0deg)",
+  },
+  to: {
+    transform: "rotate(360deg)",
+  },
+});
+const reactLogoCss = css(logoCss, {
+  "&:hover": {
+    filter: "drop-shadow(0 0 2em #61dafbaa)",
+  },
+  animation: `${reactLogoSpin} infinite 20s linear`,
+});
+const viteLogoCss = css(logoCss, {
+  "&:hover": {
+    filter: "drop-shadow(0 0 2em #646cffaa)",
+  },
+});
+const cardCss = css({
+  padding: "2em",
+});
+const readTheDocsCss = css({
+  color: "#888",
+});
 
 export function Sample() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div css={rootCss}>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/vite.svg" css={viteLogoCss} alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} css={reactLogoCss} alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div css={cardCss}>
         <button
           type="button"
           onClick={() => setCount((countNumber) => countNumber + 1)}
@@ -27,7 +64,7 @@ export function Sample() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p css={readTheDocsCss}>
         Click on the Vite and React logos to learn more
       </p>
     </div>
